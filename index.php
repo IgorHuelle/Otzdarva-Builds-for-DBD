42c93bd912b7469e25f6943fce9df8f8391a5816
33jarek <33patryk.jarosz@gmail.com>
2025-05-19 00:03:10 +0200

    upd

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OTZ BUILDS FOR KILLER/SURVIVIOR</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="resources/favicon.png" type="image/png">
+    <meta name="keywords" content="otzdarva, builds, dead by daylight, dbd, killer builds, survivor builds, dbd builds, recommended builds, otzdarva builds, dbd tierlist">
+</head>
+<body>
+    <nav>
+        <a href="https://www.twitch.tv/otzdarva" target="_blank"><img src="resources\twitch.png" alt="ttv"></a>
+        <a href="https://www.discord.gg/otzdarva" target="_blank"><img src="resources\discord.png" alt="dc"></a>
+        <a href="https://www.youtube.com/@notOtzdarva" target="_blank"><img src="resources\youtube.png" alt="yt"></a>
+        <a href="https://twitter.com/otzdarvayt" target="_blank"><img src="resources\twitter.png" alt="twt"></a>
+    </nav>
+    <header>
+        <h1>OTZDARVA RECOMMENDED BUILDS</h1>
+        <h5>LAST CHANGED: MAY 18, 2025</h5>
+    </header>
+    <main>
+        <?php
+        
+        
+        
+        ?>
+    </main>
+
+</body>
+</html>
